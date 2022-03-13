389455dfc8602815e0df8de9d8dcb42708118435
steve-128 <70624972+steve-128@users.noreply.github.com>
2022-03-13 22:14:57 +0800

    .

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "lhmethod";
+
+$search = $_POST['search'];
+  //connect to database
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=user", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //Write database
+  //$conn->exec(variable or code)
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+};
+?>
