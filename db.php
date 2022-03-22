aaaa5f274d97452487032d70fad5f6812ae27ac9
steve-128 <70624972+steve-128@users.noreply.github.com>
2022-03-22 14:53:17 +0800

    .

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <!-- connect to database -->
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "lhmethod";
+
+$hostname = "us-cdbr-east-05.cleardb.net";
+$username = "b5360e8317bffd";
+$password = "bff46df7";
+$database = "heroku_9e9ee605a716345";
   //connect to database
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=user", $username, $password);
+  $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "connected";
   //Write database
   //$conn->exec(variable or code)
 } catch(PDOException $e) {
--- index.php
+++ index.php
@@ -4,23 +4,7 @@
     <a href='log.php'>Log In</a> |
     <a href='help.php'>HELP</a>
 </nav>
-
 <?php
 echo "Hi! This is an app that is similar to instagram but not as good as instagram!";
-echo "<br>"; 
-
-$hostname = "us-cdbr-east-05.cleardb.net";
-$username = "b5360e8317bffd";
-$password = "bff46df7";
-$database = "heroku_9e9ee605a716345";
-  //connect to database
-try {
-  $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //Write database
-  //$conn->exec(variable or code)
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-};
+echo "<br>";
 ?>
