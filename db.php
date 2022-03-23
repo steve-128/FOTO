7248c9f26a0423659b80e3e31051553dd7a5193c
steve-128 <70624972+steve-128@users.noreply.github.com>
2022-03-23 13:43:07 +0800

    .

--- db.php
+++ db.php
@@ -10,7 +10,6 @@ try {
   $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "connected";
   //Write database
   //$conn->exec(variable or code)
 } catch(PDOException $e) {
