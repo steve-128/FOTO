fefb9614e721dadc1a3ef87b3464a38ca0aef5a5
steve-128 <70624972+steve-128@users.noreply.github.com>
2022-03-29 13:48:47 +0800

    .

--- file.php
+++ file.php
@@ -1,10 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        require("db.php");
+    ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://app.simplefileupload.com/buckets/807627ca14520b5bbb91e89b80283b37.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form action = "" method = "post" enctype = "multipart/form-data">
+        <input type="hidden" name="avatar_url" id="avatar_url" class="simple-file-upload">
+        <input type="submit" name="submit" id="submit">
+    </form>
+</body>
+</html>
+<html>
 <!-- upload post form handler -->
-<?php
-    require('db.php');
-?>
-<?php
+
+<!-- <?php
     session_start();
     
+    
+
+
     echo "Your post:<br>";
     require('movefile.php');
     echo $_POST['description'];
@@ -13,4 +34,4 @@
     $conn->exec($sql);
 
     header("refresh: 1;url=mypage.php");
-?>
+?> -->
